ca1ae02a237d4ffe78771aedfec0974d36e1a8c1
alexandre.vigneron <alexandre.vigneron@docaposte.fr>
2025-09-15 10:49:42 +0200

    Tri des articles par date

--- src/components/CardArticle.tsx
+++ src/components/CardArticle.tsx
@@ -8,7 +8,7 @@ export const CardArticle: React.FC<{
   <div className="lg:max-w-xs group/card">
     <a href={`/actualites/${id}/`}>
       <div
-        className="cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl max-w-sm mx-auto flex flex-col justify-between p-4 bg-cover bg-center"
+        className="cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl max-w-sm mx-auto flex flex-col justify-between p-4 bg-cover bg-center grayscale-20"
         style={{ backgroundImage: `url(${article.image.url})` }}
       >
         <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-black opacity-60" />
